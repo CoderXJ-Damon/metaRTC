1f18e4adc753ab75762f886046674737ed910c3f
CoderXJ-Damon <xiaojiedamon@gmail.com>
2026-04-28 02:55:51 -0700

    添加交叉编译

--- arch/aarch64.cmake
+++ arch/aarch64.cmake
@@ -1,3 +1,3 @@
 SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_C_COMPILER /opt/arm64/aarch64-linux-gnu-gcc)
-SET(CMAKE_CXX_COMPILER /opt/arm64/aarch64-linux-gnu-g++)
+SET(CMAKE_C_COMPILER /home/tplink/master/sdk/soc/ssc388/glibc-toolchain-12.4.0/arm-unknown-linux-gcc-12.4.0-glibc-2.37-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /home/tplink/master/sdk/soc/ssc388/glibc-toolchain-12.4.0/arm-unknown-linux-gcc-12.4.0-glibc-2.37-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++)
